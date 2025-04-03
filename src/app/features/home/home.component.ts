f2db93352e9dbbaf811551c59d39a081e8d93801
Cédric Baumann <cedric.baumann94@gmail.com>
2025-04-03 16:54:26 +0200

    optimized recipe generation, added styling

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatButton],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
